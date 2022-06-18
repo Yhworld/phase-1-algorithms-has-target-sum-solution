ae5aca5f5d74ebd20ed4a037820c1e1669aaa46b
Yahya <yahyahussein670@gmail.com>
2022-06-18 09:23:48 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
   for (let i = 0; i < array.length; i++) {
     const complement = target - array[i]
-    for (let j = i+1; j < array.length; j++) {
-      if (array[j] === complement) return true;
+     if (seenNumbers[complement]) return true;
+     seenNumbers[array[i]] = true;
   }
-}
 return false;
 }
-console.log(hasTargetSum([20,20,40], 50))
+
+hasTargetSum([20,35,25], 50);
 
 /* 
   Write the Big O time complexity of your function here
@@ -18,10 +19,11 @@ console.log(hasTargetSum([20,20,40], 50))
   Add your pseudocode here
   1. hasTargetSum(array, target)
   2. iterates throught each number in the array
-  3. for the current num identify a complement that adds upto the target value
-  4. iterate throught the array again
-  5. if the value achieved is our complement return true 
-  6. else if its not achieved return false
+  3. Add an object which stores the keys of the seen numbers
+  4. for the current num identify a complement that adds upto the target value
+  5. iterate throught the array again
+  6. if the value achieved is our complement return true 
+  7. else if its not achieved return false
 */
 /*
   Add written explanation of your solution here
@@ -44,4 +46,4 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
-module.exports = hasTargetSum;
+module.exports = hasTargetSum; 
